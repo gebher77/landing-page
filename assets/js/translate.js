f5cb81b618ac7372cfb259fb1a11250ff2650635
Gabriel Melo <melo90350@gmail.com>
2024-09-22 00:38:14 -0300

    improved translation speed and added details

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,34 +1,29 @@
 // Substitua pela sua chave de API do Google Cloud Translate.
-const API_KEY = "SUA CHAVE API";
-
+const API_KEY = 'API KEY';
 
 async function translateText(text, targetLanguage) {
   const url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
-  const data = {
-    q: text,
-    target: targetLanguage,
-  };
+  const data = { q: text, target: targetLanguage };
+  
   try {
     const response = await fetch(url, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
+    
     const result = await response.json();
-    if (result.data && result.data.translations) {
+    if (result.data && result.data.translations && result.data.translations.length > 0) {
       return result.data.translations[0].translatedText;
     } else {
       console.error("Erro na resposta de tradução:", result);
-      return text; // Retorna o texto original se houver erro
+      return text;
     }
   } catch (error) {
     console.error("Erro na requisição:", error);
-    return text; // Retorna o texto original se houver erro
+    return text;
   }
 }
-
 
 function decodeHTMLEntities(text) {
   const textarea = document.createElement("textarea");
@@ -36,24 +31,18 @@ function decodeHTMLEntities(text) {
   return textarea.value;
 }
 
-
 async function translateHTMLContent(targetLanguage) {
   const elementsToTranslate = document.querySelectorAll(
     'h1, h2, h3, p, span, a, label, input[type="submit"], button, input[placeholder], textarea[placeholder]'
   );
 
-  for (let element of elementsToTranslate) {
-    const originalText =
-      element.innerText || element.placeholder || element.value;
+  const translationPromises = Array.from(elementsToTranslate).map(async (element) => {
+    const originalText = element.innerText || element.placeholder || element.value;
     if (originalText && originalText.trim()) {
-
       const translatedText = await translateText(originalText, targetLanguage);
       const decodedText = decodeHTMLEntities(translatedText);
       if (decodedText) {
-        if (
-          element.tagName.toLowerCase() === "input" &&
-          element.type === "submit"
-        ) {
+        if (element.tagName.toLowerCase() === "input" && element.type === "submit") {
           element.value = decodedText;
         } else if (element.tagName.toLowerCase() === "button") {
           element.innerText = decodedText;
@@ -64,9 +53,10 @@ async function translateHTMLContent(targetLanguage) {
         }
       }
     }
-  }
-}
+  });
 
+  await Promise.all(translationPromises);
+}
 
 function updateImages(targetLanguage) {
   const images = document.querySelectorAll("img[data-en]");
@@ -79,14 +69,12 @@ function updateImages(targetLanguage) {
   });
 }
 
-
 function handleLanguageChange(event) {
   const selectedLanguage = event.target.value;
   let targetLanguage = selectedLanguage === "EN" ? "en" : "pt";
   translateHTMLContent(targetLanguage);
   updateImages(targetLanguage);
 }
-
 
 document
   .getElementById("select-language")
